5435bde6abcbb15cb5d504f1390ce2cf93793a82
zutken <b.alzamora@gmail.com>
2017-04-29 22:21:36 +1000

    working until get location

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 # Client Access Token for accessing our API AI Bot
 CLIENT_ACCESS_TOKEN = '64eeb868e8744855bb32717253fdaed8'
 # Page Access Token for Facebook Page where the conversation can be started with the bot.
-PAGE_ACCESS_TOKEN = 'EAAIQYGsJQxwBAJT6sWAFuzXy3RyceQaqO7yszXzj7fGKPSr0GEcczmIH6sr4rBsrZBesgsbwlRuLtB6j8ASwu86fSQskfT7HEbjzINAP8SxHqRhWGiAVJOMBZCJKOczHEbVjY61m3Y08vpZByOwjjLv0pwmydijSdl6hSd5xQZDZD'
+PAGE_ACCESS_TOKEN = 'EAAa2h6P2zZC4BAJYqEVm4vrHZAEKVxcFeZAisoCfztQxAmEjJJg2vNG1IIZBCFEfaoPUQzL7Fe0jwqpgbyYPEw6C7g6xeQUjTrTZBIPuAhpL5Xq4QItMH4jGtFFYvjMI3cRTKPAQjoiOgVKE5QRg4xgCYhY3o5ZCEEb2wyVuHFZBwZDZD'
 # Token created whilst configuring Webhook subscription.
 VERIFY_TOKEN = 'abc123'
