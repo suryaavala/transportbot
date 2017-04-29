c2b878b1032b84aa89fdc86eebd9f390589b8588
zutken <b.alzamora@gmail.com>
2017-04-29 18:18:42 +1000

    initial code

---
+++ config.py
@@ -0,0 +1,7 @@
+
+# Client Access Token for accessing our API AI Bot
+CLIENT_ACCESS_TOKEN = '64eeb868e8744855bb32717253fdaed8'
+# Page Access Token for Facebook Page where the conversation can be started with the bot.
+PAGE_ACCESS_TOKEN = 'EAAIQYGsJQxwBAJT6sWAFuzXy3RyceQaqO7yszXzj7fGKPSr0GEcczmIH6sr4rBsrZBesgsbwlRuLtB6j8ASwu86fSQskfT7HEbjzINAP8SxHqRhWGiAVJOMBZCJKOczHEbVjY61m3Y08vpZByOwjjLv0pwmydijSdl6hSd5xQZDZD'
+# Token created whilst configuring Webhook subscription.
+VERIFY_TOKEN = 'abc123'
---
+++ threadsettings.py
@@ -0,0 +1,113 @@
+###############################################################################
+# https://developers.facebook.com/docs/messenger-platform/thread-settings
+###############################################################################
+import os
+import sys
+import json
+import requests
+from config import *
+
+def send_greetings():
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "setting_type": "greeting",
+        "greeting": {
+            "text": "G'day {{user_first_name}} I'm TransportBot! I'm ready to help you get real time directions in NSW."
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/thread_settings", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
+
+
+def get_started():
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "setting_type": "call_to_actions",
+        "thread_state": "new_thread",
+        "call_to_actions": [
+            {
+                "payload": "Successfully added new_thread's CTAs"
+            }
+        ]
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/thread_settings", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
+
+
+def delete_get_started(recipient_id):
+    print("====================> NEW USER: delete getting started for user {recipient}".format(recipient=recipient_id))
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "setting_type": "call_to_actions",
+        "thread_state": "new_thread"
+    })
+    r = requests.delete("https://graph.facebook.com/v2.6/me/thread_settings", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
+
+
+def show_persistent_menu():
+    print("show persistent menu for user")
+
+    params = {
+        "access_token": PAGE_ACCESS_TOKEN
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "setting_type": "call_to_actions",
+        "thread_state": "existing_thread",
+        "call_to_actions": [
+            {
+                "type": "postback",
+                "title": "Show buses only",
+                "payload": "MENU_ALL"
+            },
+            {
+                "type": "postback",
+                "title": "Show trains now",
+                "payload": "MENU_FAV"
+            },
+            {
+                "type": "postback",
+                "title": "Show everything",
+                "payload": "MENU_FAV"
+            },
+            {
+                "type": "web_url",
+                "title": "Like Facebook Page",
+                "url": "https://www.facebook.com/eventochatbot/"
+            },
+            {
+                "type": "postback",
+                "title": "Help",
+                "payload": "MENU_HELP"
+            },
+        ]
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/thread_settings", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        print(r.status_code)
+        print(r.text)
