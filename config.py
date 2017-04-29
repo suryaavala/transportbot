23d943eedfbbcb63c9cfa77b67238b5765aabf07
Surya Avala <suryaavala.dev@gmail.com>
2017-04-29 19:13:05 +1000

    aded transportnsw api key to config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ CLIENT_ACCESS_TOKEN = '64eeb868e8744855bb32717253fdaed8'
 PAGE_ACCESS_TOKEN = 'EAAIQYGsJQxwBAJT6sWAFuzXy3RyceQaqO7yszXzj7fGKPSr0GEcczmIH6sr4rBsrZBesgsbwlRuLtB6j8ASwu86fSQskfT7HEbjzINAP8SxHqRhWGiAVJOMBZCJKOczHEbVjY61m3Y08vpZByOwjjLv0pwmydijSdl6hSd5xQZDZD'
 # Token created whilst configuring Webhook subscription.
 VERIFY_TOKEN = 'abc123'
+
+TRANSPORT_API_KEY = 'Bqe7emwqVCWh4bdhB8v7yQOjozpkUkt2Aesx'
